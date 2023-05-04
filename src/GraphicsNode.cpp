dabd151784487bb389dc788e38ea8b2dd07cc851
Pikachuxxxx <Phani.s2909@icloud.com>
2023-05-04 23:07:08 +0530

    Added test node base

--- src/GraphicsNode.cpp
+++ src/GraphicsNode.cpp
@@ -4,14 +4,54 @@
 #include "NodeScene.h"
 
 GraphicsNode::GraphicsNode(Node* node, std::string title)
+    : node(node)
 {
+    this->setFlags(QGraphicsItem::ItemIsMovable | QGraphicsItem::ItemIsSelectable);
     this->setPos(node->getScene()->getOrigin().x(), node->getScene()->getOrigin().y());
     titleItem = new QGraphicsTextItem(this);
     titleItem->setPlainText(title.c_str());
     titleItem->setDefaultTextColor(Qt::white);
+    titleItem->setTextWidth(width - 2 * padding);
+    titleItem->setPos(padding, 0);
+
+    penDefault = QPen(QColor("#7f000000"));
+    penDefault.setWidth(2);
+    penSelected = QPen(QColor("#FFFFA637"));
+    penSelected.setWidth(2);
+
+    titleBrush = QBrush(QColor("#FF313131"));
+    bgBrush = QBrush(QColor("#E3212121"));
 }
 
 void GraphicsNode::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
-    
+    // Title
+    auto path_title = QPainterPath();
+    path_title.setFillRule(Qt::WindingFill);
+    path_title.addRoundedRect(0, 0, width, titleHeight, edge_size, edge_size);
+    path_title.addRect(0, titleHeight - edge_size, edge_size, edge_size);
+    path_title.addRect(width - edge_size, titleHeight - edge_size, edge_size, edge_size);
+    painter->setPen(Qt::NoPen);
+    painter->setBrush(titleBrush);
+    painter->drawPath(path_title.simplified());
+
+    // Content
+    auto path_content = QPainterPath();
+    path_content.setFillRule(Qt::WindingFill);
+    path_content.addRoundedRect(0, titleHeight, width, height - titleHeight, edge_size, edge_size);
+    path_content.addRect(0, titleHeight, edge_size, edge_size);
+    path_content.addRect(width - edge_size, titleHeight, edge_size, edge_size);
+    painter->setPen(Qt::NoPen);
+    painter->setBrush(bgBrush);
+    painter->drawPath(path_content.simplified());
+
+    // Outline
+    auto path_outline = QPainterPath();
+    path_outline.addRoundedRect(0, 0, width, height, edge_size, edge_size);
+    if(!isSelected())
+        painter->setPen(penDefault);
+    else
+        painter->setPen(penSelected);
+    painter->setBrush(Qt::NoBrush);
+    painter->drawPath(path_outline.simplified());
 }
--- src/GraphicsNode.h
+++ src/GraphicsNode.h
@@ -2,6 +2,7 @@
 
 #include <QGraphicsItem>
 #include <QGraphicsTextItem>
+#include <QPen>
 
 class Node;
 
@@ -11,11 +12,22 @@ public:
     GraphicsNode(Node* node, std::string title);
     ~GraphicsNode() {}
 
-    QRectF boundingRect() const override { return QRectF(0,0,100,100); }
+    QRectF boundingRect() const override { return QRectF(0,0,2*edge_size + width,2*edge_size+height).normalized(); }
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = nullptr) override;
 
 private:
     Node* node;
     QColor titleColor;
     QGraphicsTextItem* titleItem;
+    uint32_t width = 180;
+    uint32_t height = 240;
+    uint32_t edge_size = 10;
+    uint32_t titleHeight = 24;
+    uint32_t padding = 5;
+    QPen penDefault;
+    QPen penSelected;
+    QBrush titleBrush;
+    QBrush bgBrush;
+
+
 };
